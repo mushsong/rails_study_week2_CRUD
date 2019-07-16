eb0481944ce7fdbbf5a06e8a309537d3ee9e30ff
mushsong <yangsongyiofficial@gmail.com>
2019-07-17 00:53:52 +0900

    add class method, instance method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,12 @@ class Post < ApplicationRecord
     belongs_to :user
 
     def self.generate_posts
-        Post.create!(title: "제목입니다.", content: "내용입니다.")
+        Post.create!(title: "제목입니다.",
+                    content: "내용입니다.",
+                    user_id: User.first.id)
+    end
+
+    def say_posts
+        puts("#{self.title}를 제목으로, #{self.content}를 내용으로 입력하시겠습니까?")
     end
 end
